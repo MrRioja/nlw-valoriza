01b8ad28a6111730bdb18a15c542695f9c835502
Luiz Rioja <lulyrioja@gmail.com>
2021-06-23 00:10:04 -0300

    :alien: Creating error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+
 import { routes } from "./routes";
 import "./database";
 
 const app = express();
 
 app.use(express.json());
-app.listen(3333);
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
+
+app.listen(3333);
