d779205cfc37dbef7372ff781eb1524322e9f418
Luiz Rioja <lulyrioja@gmail.com>
2021-06-23 23:58:26 -0300

    :alien: Creating tags and compliments route

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,20 @@
 import { Router } from "express";
-import { CreateUserController } from "./controllers/CreateUserController";
-import { CreateTagController } from "./controllers/CreateTagController";
-import { ensureAdmin } from "./middlewares/ensureAdmin";
 const routes = Router();
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
-const createUserController = new CreateUserController();
+import { CreateTagController } from "./controllers/CreateTagController";
+import { CreateUserController } from "./controllers/CreateUserController";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
+
 const createTagController = new CreateTagController();
+const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
+routes.post("/tags", createTagController.handle);
 routes.post("/users", createUserController.handle);
-
-routes.post("/tags", ensureAdmin, createTagController.handle);
+routes.post("/login", authenticateUserController.handle);
+routes.post("/compliments", createComplimentController.handle);
 
 export { routes };
