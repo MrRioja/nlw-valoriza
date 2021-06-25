4f17f9ba2848eca451f810045e1a8807cb23e2cc
Luiz Rioja <lulyrioja@gmail.com>
2021-06-25 00:58:20 -0300

    :alien: Adding a column in entity tags

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -1,4 +1,5 @@
 import { v4 as uuid } from "uuid";
+import { Expose } from "class-transformer";
 import {
   Column,
   CreateDateColumn,
@@ -6,7 +7,6 @@ import {
   PrimaryColumn,
   UpdateDateColumn,
 } from "typeorm";
-
 @Entity("tags")
 class Tag {
   @PrimaryColumn()
@@ -20,6 +20,11 @@ class Tag {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: "name_custom" })
+  nameCustom(): string {
+    return `#${this.name}`;
+  }
 
   constructor() {
     if (!this.id) {
